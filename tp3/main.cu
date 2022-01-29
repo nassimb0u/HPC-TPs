4d015a1ca8a15a0c7f96c81f7b10500e3bf40aec
nassimb0u <in_boulechfar@esi.dz>
2022-01-29 14:21:14 +0100

    Fix using flat 2d array for first approach
    (main.cu)

--- tp3/main.cu
+++ tp3/main.cu
@@ -4,13 +4,13 @@
 #define N 16
 #define M 16
 
-__global__ void prod(int **a, int *b, int *c) {
+__global__ void prod(int *a, int *b, int *c) {
     const int t = 16;
     __shared__ int tmp[t];
     int i = (blockIdx.y * blockDim.y + threadIdx.y) * 2;
     int j = (blockIdx.x * blockDim.x + threadIdx.x) * 2;
-    tmp[i] = a[i][j] * b[j] + a[i][j + 1] * b[j + 1];
-    tmp[i + 1] = a[i + 1][j] * b[j] + a[i + 1][j + 1] * b[j + 1];
+    tmp[i] = a[i * M + j] * b[j] + a[i * M + j + 1] * b[j + 1];
+    tmp[i + 1] = a[(i + 1) * M + j] * b[j] + a[(i + 1) * M + j + 1] * b[j + 1];
     __syncthreads();
     if (threadIdx.x == 0 && threadIdx.y == 0) {
         for (int k = 0; k < 4; k++) atomicAdd(&c[i + k], tmp[i + k]);
@@ -18,8 +18,8 @@ __global__ void prod(int **a, int *b, int *c) {
 }
 
 int main() {
-    int **a, *b, *c;
-    int **d_a, *d_b, *d_c;
+    int *a, *b, *c;
+    int *d_a, *d_b, *d_c;
     dim3 grid(4, 4);
     dim3 block(2, 2);
     // variables pour le calcule du temps
@@ -28,23 +28,18 @@ int main() {
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     // allocation
-    a = (int **)malloc(N * sizeof(int *));
-    for (int i = 0; i < N; i++) a[i] = (int *)malloc(M * sizeof(int));
+    a = (int *)malloc(N * M * sizeof(int));
     b = (int *)malloc(M * sizeof(int));
     c = (int *)malloc(N * sizeof(int));
     // init
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) a[i][j] = i == j;
-    }
+    for (int i = 0; i < N; i++) a[i][j] = 1;
     for (int i = 0; i < M; i++) b[i] = i;
     // allocation mem GPU
-    cudaMalloc((void **)&d_a, N * sizeof(int *));
-    for (int i = 0; i < N; i++) cudaMalloc((void **)&d_a[i], M * sizeof(int));
+    cudaMalloc((void **)&d_a, N * M * sizeof(int));
     cudaMalloc((void **)&d_b, M * sizeof(int));
     cudaMalloc((void **)&d_c, N * sizeof(int));
     // cpy data
-    for (int i = 0; i < N; i++)
-        cudaMemcpy(d_a[i], a[i], M * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, a, M * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, M * sizeof(int), cudaMemcpyHostToDevice);
     // call kernel
     cudaEventRecord(start, 0);
@@ -60,12 +55,10 @@ int main() {
     for (int i = 0; i < N; i++) printf("%d ", c[i]);
     printf("\n");
     // free mem gpu
-    for (int i = 0; i < N; i++) cudaFree(d_a[i]);
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
     // free mem
-    for (int i = 0; i < N; i++) free(a[i]);
     free(a);
     free(b);
     free(c);
